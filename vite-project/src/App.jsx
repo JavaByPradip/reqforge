6bae34c824c74ee0bbd61785ce11421d420716b7
JavaByPradip <pradipmandalraj95@gmail.com>
2026-03-23 17:39:11 +0530

    Added POST and body support

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,14 +4,22 @@ import axios from "axios";
 function App() {
   const [url, setUrl] = useState("");
   const [method, setMethod] = useState("GET");
+  const [headers, setHeaders] = useState("");
+  const [body, setBody] = useState("");
   const [response, setResponse] = useState("");
 
   const sendRequest = async () => {
     try {
+      const parsedHeaders = headers ? JSON.parse(headers) : {};
+      const parsedBody = body ? JSON.parse(body) : {};
+
       const res = await axios({
         url,
-        method
+        method,
+        headers: parsedHeaders,
+        data: parsedBody
       });
+
       setResponse(JSON.stringify(res.data, null, 2));
     } catch (err) {
       setResponse(err.toString());
@@ -21,7 +29,6 @@ function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>🔥 ReqForge</h1>
-      <p>Forge Your API Requests</p>
 
       <select onChange={(e) => setMethod(e.target.value)}>
         <option>GET</option>
@@ -36,6 +43,22 @@ function App() {
         style={{ width: "500px" }}
         placeholder="Enter API URL"
         onChange={(e) => setUrl(e.target.value)}
+      />
+
+      <br /><br />
+
+      <textarea
+        style={{ width: "500px", height: "80px" }}
+        placeholder='Headers (JSON format)'
+        onChange={(e) => setHeaders(e.target.value)}
+      />
+
+      <br /><br />
+
+      <textarea
+        style={{ width: "500px", height: "100px" }}
+        placeholder='Body (JSON format)'
+        onChange={(e) => setBody(e.target.value)}
       />
 
       <br /><br />
